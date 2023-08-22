134ee56a23ac171e61af89eebfde5485759d0e08
Guilherme <guilherme.reiskill2002@gmail.com>
2023-08-22 17:07:32 -0300

    API finalizada

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -7,6 +7,9 @@ const notesRoutes = Router()
 const notesController = new NotesController()
 
 notesRoutes.post('/:user_id', notesController.create)
+notesRoutes.get('/:id', notesController.show)
+notesRoutes.delete('/:id', notesController.delete)
+notesRoutes.get('/', notesController.index)
 
 
 module.exports = notesRoutes
